1f700b8332f9661df52f7a551461d45c4c66ea6b
ck-kor <ckdrb48@naver.com>
2022-06-13 10:26:16 +0900

    modify SignUpRequestDto

--- src/main/java/com/sparta/week6project/dto/requestDto/SignupRequestDto.java
+++ src/main/java/com/sparta/week6project/dto/requestDto/SignupRequestDto.java
@@ -1,11 +1,20 @@
 package com.sparta.week6project.dto.requestDto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
 @Getter
-public class SignupRequestDto {
-
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignUpRequestDto {
     private String username;
     private String password;
+    private String passwordCheck;
+    private String profileImage;
+    private String email;
     private String nickname;
+
 }
